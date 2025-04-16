5975f81f1af6a7c5783ace7c9a3e7ea060816296
Cesar <cesarmu@yahoo.com>
2025-04-15 23:25:03 -0600

    otro

--- WebApiDePrueba/Controllers/WeatherForecastController.cs
+++ WebApiDePrueba/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApiDePrueba.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Other"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
